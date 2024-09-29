eff90a117739d54bc094a2fe91525f8de19f9d05
val2cak <valentina.tucak99@gmail.com>
2024-09-29 17:32:48 +0200

    fix: categories bar

--- src/components/categories-bar/categories-bar.tsx
+++ src/components/categories-bar/categories-bar.tsx
@@ -1,24 +1,73 @@
-import { Fragment } from 'react/jsx-runtime';
-import { Link } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import { categories } from '../../constants/categories';
 
 const CategoriesBar = () => {
+  const [openCategory, setOpenCategory] = useState<string | null>(null);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const categoryFromQuery = queryParams.get('category');
+
+  const toggleDropdown = (categorySlug: string) => {
+    setOpenCategory(openCategory === categorySlug ? null : categorySlug);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setOpenCategory(null);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className='w-full bg-dark text-light py-4 sm:px-8 lg:px-16 px-40 uppercase text-base font-righteous'>
-      <div className='container mx-auto'>
+    <div
+      ref={dropdownRef}
+      className='w-full bg-dark text-light py-4 sm:px-8 lg:px-16 px-40 uppercase text-base font-righteous z-30'
+    >
+      <div className='mx-auto'>
         <ul className='flex justify-between items-center gap-8'>
-          {categories.map((category, index) => (
-            <Fragment key={index}>
-              <li className='tracking-wider'>
-                <Link
-                  to={`/products?category=${category.slug}`}
-                  className='hover:text-primary'
-                >
-                  {category.name}
-                </Link>
-              </li>
-            </Fragment>
+          {categories.map((category) => (
+            <li key={category.slug} className='relative'>
+              <button
+                className='hover:text-primary uppercase px-4'
+                onClick={() => toggleDropdown(category.slug)}
+              >
+                {category.name}
+              </button>
+              {openCategory === category.slug && (
+                <div className='absolute left-0 mt-2 w-56 bg-dark text-light shadow-md'>
+                  <ul className='py-2'>
+                    {category.children.map((child) => (
+                      <li key={child.slug}>
+                        <Link
+                          to={`/products?category=${child.slug}`}
+                          className={`block px-4 py-2  ${
+                            categoryFromQuery === child.slug
+                              ? 'text-primary'
+                              : 'hover:text-primary'
+                          }`}
+                          onClick={() => setOpenCategory(null)}
+                        >
+                          {child.name}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </li>
           ))}
         </ul>
       </div>
