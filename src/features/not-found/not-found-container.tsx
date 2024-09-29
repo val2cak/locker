33c490db05b5305365f6f5d3052ac3fc734b3509
val2cak <valentina.tucak99@gmail.com>
2024-09-29 15:38:44 +0200

    feat: not found page

--- src/features/not-found/not-found-container.tsx
+++ src/features/not-found/not-found-container.tsx
@@ -1,5 +1,36 @@
+import { useNavigate } from 'react-router-dom';
+
+import Button from '../../components/button/button';
+import locale from '../../localization/locale';
+import Logo from '../../components/logo/logo';
+
 const NotFoundContainer = () => {
-  return <div>NotFoundContainer</div>;
+  const { btnText, subtitle, title } = locale.pageNotFound;
+
+  const navigate = useNavigate();
+
+  const navigateToHome = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className='flex flex-col justify-center items-center gap-8 h-full min-h-screen bg-light'>
+      <div className='flex flex-col gap-2 items-center'>
+        <div className='scale-150 pb-4'>
+          <Logo size={'w-40'} />
+        </div>
+        <span className='sm:text-lg text-2xl uppercase font-righteous'>
+          {title}
+        </span>
+        <span className='text-md font-medium'>{subtitle}</span>
+      </div>
+      <Button
+        text={btnText}
+        handleOnClick={navigateToHome}
+        className='!bg-primary'
+      />
+    </div>
+  );
 };
 
 export default NotFoundContainer;
